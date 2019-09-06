3d0c5e9a197752130e55d4fc1329960b5a210ef7
MarioCesarJr <mcsjuniorsouza@gmail.com>
2019-09-06 10:51:38 -0300

    seed user

--- backend/app/Models/Team.js
+++ backend/app/Models/Team.js
@@ -1,14 +1,26 @@
-'use strict';
+'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model');
+const Model = use('Model')
 
 class Team extends Model {
-  user() {
+  static boot () {
+    super.boot()
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name'
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: false
+    })
+  }
+
+  user () {
     return this.belongsToMany('App/Models/User').pivotModel(
       'App/Models/UserTeam'
-    );
+    )
   }
 }
 
-module.exports = Team;
+module.exports = Team
