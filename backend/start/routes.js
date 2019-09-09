d0a53dcf8a116f42888e0c1094bc401c55dddbab
MarioCesarJr <mcsjuniorsouza@gmail.com>
2019-09-09 13:56:26 -0300

    list, edit members

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -59,4 +59,7 @@ Route.group(() => {
         ]
       )
     )
+
+  Route.get('members', 'MemberController.index')
+  Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
 }).middleware(['auth', 'team'])
