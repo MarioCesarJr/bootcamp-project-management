085808f93a0e65d6e2aadd040187f8e7cad81eeb
MarioCesarJr <mcsjuniorsouza@gmail.com>
2019-09-16 09:55:57 -0300

    members list

---
+++ frontend/src/store/sagas/members.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-cycle */
+import { call, put } from 'redux-saga/effects';
+import api from '~/services/api';
+
+import MembersActions from '../ducks/members';
+
+export function* getMembers() {
+  const response = yield call(api.get, 'members');
+
+  yield put(MembersActions.getMembersSuccess(response.data));
+}
